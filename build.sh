0c0d5479c0a9c86b8b5b439aab58ee1581ed1db5
Yongwei Yuan <yuan311@purdue.edu>
2023-03-03 17:28:19 -0500

    use burst makefile

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ fi
 opam switch create $OCAML_VERSION
 eval $(opam env)
 opam install --yes dune qcheck z3 ppx_inline_test core core_unix iter bark menhir.20211128 ppx_deriving smart-print async_unix js_of_ocaml-ppx
-(cd burst && dune build app/BurstCmdLine.exe --profile release)
+(cd burst && make)
 (cd smyth && dune build src/main.exe)
 (cd syrup && dune build)
